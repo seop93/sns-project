a16222e02c7012c2b95daacc957bf433a7748187
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 21:58:42 +0900

    Feat: 클라이언트에게 보내줄 데이터 dto 정의

---
+++ src/main/java/com/practice/snsproject/doamin/dto/response/UserJoinResponse.java
@@ -0,0 +1,11 @@
+package com.practice.snsproject.doamin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserJoinResponse {
+    private Integer userId;
+    private String userName;
+}
