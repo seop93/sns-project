45c48d39a988ee6560fe7013b2ed9cd4a0c97e23
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 21:58:08 +0900

    Feat: UserController 정의

---
+++ src/main/java/com/practice/snsproject/controller/api/UserApiController.java
@@ -0,0 +1,30 @@
+package com.practice.snsproject.controller.api;
+
+
+import com.practice.snsproject.doamin.dto.request.UserJoinRequest;
+import com.practice.snsproject.doamin.dto.response.Response;
+import com.practice.snsproject.doamin.dto.response.UserJoinResponse;
+import com.practice.snsproject.doamin.entity.User;
+import com.practice.snsproject.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("api/v1/users")
+@RestController
+public class UserApiController {
+
+    private final UserService userService;
+
+    @ApiOperation(value = "회원가입", notes = "사용자가 입력한 아이디와 비밀번호로 회원가입을 한다")
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request){
+        User savedUser = userService.join(request.getUserName(), request.getPassword());
+        return Response.success(new UserJoinResponse(savedUser.getId(), savedUser.getUserName()));
+
+    }
+}
