ba518f1c04c83983d388d16d6782b2539412d44e
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 21:57:52 +0900

    Feat: User엔티티

---
+++ src/main/java/com/practice/snsproject/doamin/entity/User.java
@@ -0,0 +1,34 @@
+package com.practice.snsproject.doamin.entity;
+
+import com.practice.snsproject.enumerate.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "user_id")
+    private Integer id;
+
+    @Column(unique = true)
+    private String userName;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public static User registerUser(String userName, String password){
+        return User.builder()
+                .userName(userName)
+                .password(password)
+                .role(UserRole.USER)
+                .build();
+    }
+}
