3c901dac8af55ba7cd61bf5bcc9d1a457b834907
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 21:59:37 +0900

    Feat: UserRole마다 기능을 나누기 위해 enum으로 역할 구분

---
+++ src/main/java/com/practice/snsproject/enumerate/UserRole.java
@@ -0,0 +1,13 @@
+package com.practice.snsproject.enumerate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserRole {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+}
