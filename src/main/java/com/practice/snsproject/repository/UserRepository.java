57bf08f97b2e74047da4705f38b847ce29ab36f4
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-12 16:46:15 +0900

    Refactor: 리포지토리 userName으로 사용자 찾을 수있는 함수 구현

--- src/main/java/com/practice/snsproject/repository/UserRepository.java
+++ src/main/java/com/practice/snsproject/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.practice.snsproject.repository;
 
-import com.practice.snsproject.doamin.entity.User;
+import com.practice.snsproject.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
 }
